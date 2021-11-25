3e2ef7a0d0a6a9fd779acd478a96a36fa0d8905d
V-donck <tv26363@gmail.com>
2021-11-25 14:42:03 +0100

    tests Thijs nog een test

--- src/be/uantwerpen/fti/Main.java
+++ src/be/uantwerpen/fti/Main.java
@@ -3,7 +3,24 @@ package be.uantwerpen.fti;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world en thijs");
+        System.out.println("Hello world en thijssfsfsfsf");
 
+
+        /* TO DO:
+            -single  database person -> singleton
+            - singel database tickets -> singleton
+            - 2 controllers for database
+            - factory to create tickets?
+            - observer to observe databases
+            - nog design patern niet gezien in practicum
+            - GUI
+
+            - unit test
+            - integration test
+
+
+        gitkraken
+                sourcetree
+*/
     }
 }
